1ccabcd147ee878bbb19417a3a4778df34bd6c03
Hafizh Rafi M <hafizhrafi28@gmail.com>
2021-04-05 21:23:36 +0700

    Merubah Judul v_user

--- application/views/v_user.php
+++ application/views/v_user.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+    <title>Bumbumbum</title>
     <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
 </head>
 <body>
